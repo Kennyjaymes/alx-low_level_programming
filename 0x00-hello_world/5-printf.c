500ee8020aeb736560a05f75bc79238d81b5cca8
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-11 04:43:52 +0000

    kenny

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece or art,\n");
+	return (0);
+}
