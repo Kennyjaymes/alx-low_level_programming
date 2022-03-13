cf4c6a1b64ee01264dfd0ef9509a5283c60989d2
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-13 22:43:18 +0000

    kenny

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,10 @@ int main (void)
 	long long int i3;
 	char c;
 	float f;
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c)
-			printf (Size of an int: %lu byte(s)\n",(unsigned long)size of(i));
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
+			printf ("Size of an int: %lu byte(s)\n",(unsigned long)size of(i));
 				printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(i2))
-				printf(Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(i3));
+				printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(i3));
 				printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
-				return(0);}
+				return(0);
+}
