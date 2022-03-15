7f3f7fd16563402c7944be79eae15beeac4a2b01
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-15 19:10:24 +0000

    kenny

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Program entry point
+ *
+ * REturn: 0 success. Error code otherwise
+ */
+int main(void)
+{
+	char a = 'a';
+	int n = 0;
+
+	while (n < 26)
+	{
+		putchar (a);
+		a++;
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}
