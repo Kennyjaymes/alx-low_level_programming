cb9ea702be48c7cc65bab4f127a7933139ba42d5
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-16 21:08:46 +0000

    kenny

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,17 +3,20 @@
 int main(void)
 {
 char ch = 'a';
-int i = 0;    
+int i = 0;
 while (i < 16)
 {
 if (i < 10)
-{													putchar(i + '0');					
+{
+putchar(i + '0');					
 }
-else													
-{	
-putchar(ch);											
-ch++;													}
-i++;													}
+else
+}
+putchar(ch);
+ch++;
+}
+i++;
+}
 putchar('\n');
 return (0);
 }
