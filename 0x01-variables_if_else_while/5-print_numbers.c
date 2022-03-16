eb6bd27fd5140047e934844ffb639be533607738
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-16 20:37:46 +0000

    kenny

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,11 +2,12 @@
 
 int main(void)
 {
-	for (int i = 0; i <= 9; i++)
-	{
-		printf ("%d", i);
-	}
-	printf("\n");
-	return (0);
+for (int i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+
+return 0;
 }
 
