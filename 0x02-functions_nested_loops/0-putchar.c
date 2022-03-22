18800d729e00399b4f439f6ea2abfe17e7aaeb99
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-22 15:36:46 +0000

    kenny

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	char text[9] = "_putchar";
-	int j = 0;
+	int i = 0;
 
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(j);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
 	return (0);
