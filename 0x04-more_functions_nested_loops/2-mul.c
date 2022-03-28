eda3f0b1d401d149f6c1ad89e38ad411becd830d
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-28 10:39:13 +0000

    kenny

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code 
+ * main - check the code
+ * Description: function that multiplies two integers 
  * Return: Always 0.
  */
 int mul(int a, int b)
 {
 	int product;
 	product = a * b
-		return (product);
+		return (0);
 }
