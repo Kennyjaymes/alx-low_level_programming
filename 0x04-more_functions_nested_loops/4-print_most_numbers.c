328d8d150d7f777ece45eb2c79c669199eb83be8
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-28 21:31:12 +0000

    kenny

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,8 +4,16 @@
  * main - check the code
  * Return: Always 0.
  */
-int main(void)
+void print_most_numbers(void);
 {
-print_most_numbers();
-return (0);
+	char n = 0;
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar( '0' + n);
+		}
+		n++;
+	}
+	_putchar ('\n');
 }
