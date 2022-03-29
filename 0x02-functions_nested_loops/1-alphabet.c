ec6ea416efb5f51beec1f2f91539471d3d48d476
Kennyjaymes <Kennyjames104@gmail.com>
2022-03-29 22:28:16 +0000

    kenny

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,17 @@
 /**
  * main - main block
  * Description: print the alphabet in lowercase
- * Return: 0
+ * Return: void
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
 		_putchar(letter);
 		letter++;
 	}
 	_putchar('\n');
-	return (0);
+
 }
